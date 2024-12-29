7a071a6d28f4d2e453af3446c7067d182fb95931
mcyblog <mcyu815@outlook.com>
2024-12-29 03:51:47 +0000

    25 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/ff05b5bf.html","posts/a7aa354.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/ff05b5bf.html","posts/a7aa354.html","posts/kmblog5.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
